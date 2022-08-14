dabf7f69215aa42357fd5bd02fcee611f501f993
VollKornTreiber <donbeppe@hotmail.de>
2022-08-14 14:45:57 +0200

    first release with basic functions

---
+++ README.md
@@ -0,0 +1,21 @@
+#vktYT_dl_GUI
+
+-Requirements-
+	Youtube_dl library by ytdl-org: https://github.com/ytdl-org/youtube-dl
+	ffmpeg.exe + ffplay.exe + ffprobe.exe, either in directory or installed via console: 
+
+-Troubleshooting-
+
+-I'm getting an "HTTP Error 403: Forbidden".<br>
+	Just press "Start" and try again. This might happen sometimes and has to do with Youtube's servers.<br>
+
+
+Changelog:
+
+v0.5
+-Added README.md
+-Added resolution options for video download
+-Added basic exception handling
+
+WIP220813:
+-Started documenting.
--- downloader.py
+++ downloader.py
@@ -1,70 +1,112 @@
+#This downloader GUI makes use of the Youtube_dl library.
+#ffmpeg is required for this in order to convert to Mp3/mp4: https://github.com/FFmpeg/FFmpeg
+#Youtube_dl by ytdl-org: https://github.com/ytdl-org/youtube-dl
+
 from __future__ import unicode_literals;
 import youtube_dl;
 import PySimpleGUI as sg;
 
-#https://github.com/ytdl-org/youtube-dl
+VER = 0.5;
 
 print("vktYT_dl_GUI - By VollKornTreiber, 2022");
+print("This is the output window. Don't close it.");
 
 class Loader:
+
     def __init__(self):
         super().__init__();
-        sg.theme("reddit");
+        sg.theme("GreenTan");
+
+        #prepare middle row for audio/video option
+        self.col_audio = [[sg.Radio("Audio only", "FORMAT", key = "BUT_AUDIO", enable_events = True, default = True), sg.Text("Extract the audio")], [sg.Text("Bitrate: "), sg.Combo(["32","96","128","160","192","256","320"], default_value="128", key = "AUD_QUAL")]];
+        self.col_video = [[sg.Radio("Video", "FORMAT", enable_events = True, key = "BUT_VIDEO"), sg.Text("Download the entire video")], [sg.Text("Resolution: "), sg.Combo(["144","240","360","480","720","1080","1440","2160","4320"], default_value="720", key = "VID_QUAL", disabled = True)]];
+
+        #prepare whole layout
         self.layout = [
             [sg.Text("Insert your Link, choose your format and quality and download. Dead-simple!")],
-            [sg.Text("URL"), sg.Input(key = "INP"), sg.Button("Ins", key = "BUT_PASTE")],
-            [[sg.Radio("Audio only", "FORMAT", default = True), sg.Text("Extracts the audio from the video"), sg.Combo(["32","96","128","160","192","256","320"], default_value="128", key = "QUAL")], 
-             [sg.Radio("Video", "FORMAT"), sg.Text("Downloads the entire video"), sg.Combo(["144p","240p","360p","480p","720p","1080","1440p","4K","8K"], default_value="720p", key = "QUAL")]
+            [sg.Text("URL"), sg.Input(key = "INP"), sg.Button("Paste", key = "BUT_PASTE")],
+            [sg.HSeparator()],
+
+            [
+             sg.Column(self.col_audio), 
+             sg.VSeparator(),
+             sg.Column(self.col_video),
             ],
-            [sg.Button("Start!", key = "BUT_ACTION"), sg.Button("Abort!", key = "BUT_ABORT", disabled = True)],
-            [sg.Text("Ready...", key = "OUTP")],
-            [sg.ProgressBar(key = "BAR", max_value = 100, size = (50,20), visible = False)],
-            [sg.Button("Quit", key = "BUT_QUIT")],
+
+            [sg.HSeparator()],
+            [sg.Button("Start!", key = "BUT_ACTION"), sg.Button("Quit", key = "BUT_QUIT")],
             [sg.Text("GUI by VollKornTreiber, 2022")]
         ];
         
-        self.window = sg.Window("Youtube Downloader", self.layout);
+        #prepare window with title
+        self.window = sg.Window("Youtube Downloader GUI v"+str(VER), self.layout);
 
-    def Action(self, link, form, qual):
+    def Action(self):
+        
+        #called when "Start" button is pressed.
+        #Check audio/video options first
+        
+        if val["BUT_AUDIO"] == True:
+            self.settings = {
+                "format": "bestaudio/best",
+                "postprocessors": [{
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": val["AUD_QUAL"],
+                }],
+            };
 
-        self.settings = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': form,
-                'preferredquality': qual,
-            }],
-        };
+        if val["BUT_VIDEO"] == True:
+            self.settings = {
+                "format": "bestvideo[height<="+val["VID_QUAL"]+"]+bestaudio/best[height<="+val["VID_QUAL"]+"]"
+            };
+        
+        #After audio/video options check, initiate download
+
+        print("\n-------------------------------------");
+        print("Please wait... This may take a while.");
+        print("-------------------------------------");
 
         try:
             with youtube_dl.YoutubeDL(self.settings) as ydl:
-                ydl.download([link]);
+                ydl.download([val["INP"]]);
+                print("\n------------------------------------------------------");
+                print("Finished! You can download something else if you want.");
         except:
-            pass;
+            print("\n------------------------------------------------------");
+            print("Download failed! An Error has occurred! Check the log.");
+        finally:
+            
+            print("------------------------------------------------------");
 
 
-
-
+#instantiate main window
 dLoader = Loader();
 
 while True:
     event, val = dLoader.window.read();
 
     if event == sg.WINDOW_CLOSED or event == "BUT_QUIT":
+        #called when "Quit" button is pressed or window is closed
         print("Closing...");
         break;
  
     if event == "BUT_PASTE":
+        #insert from clipboard
         dLoader.window["INP"].update(sg.clipboard_get());
 
-    if event == "BUT_ACTION":
-        dLoader.window["INP"](disabled = True);
-        dLoader.window["BUT_ACTION"](disabled = True);
-        dLoader.window["BUT_PASTE"](disabled = True);
-        dLoader.window["BUT_QUIT"](disabled = True);
-        dLoader.window["BUT_ABORT"](disabled = False);
-        dLoader.window["BAR"].update(visible = True);
-        #dLoader.Action(val["INP"]);
+    if event == "BUT_AUDIO":
+        dLoader.window["AUD_QUAL"].update(disabled = False);
+        dLoader.window["VID_QUAL"].update(disabled = True);
+        
+    if event == "BUT_VIDEO":
+        dLoader.window["AUD_QUAL"].update(disabled = True);
+        dLoader.window["VID_QUAL"].update(disabled = False);
 
+    if event == "BUT_ACTION":
+        #initiate download
+        dLoader.Action();
+    
+#quit
 dLoader.window.close();
 
